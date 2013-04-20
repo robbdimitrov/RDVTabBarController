cd3c742c4e3a2d385e67db6b08567679fbd765a1
Robert Dimitrov <robert_dimitrov@me.com>
2013-04-20 11:48:56 +0300

    Initial Commit

---
+++ RDVTabBarController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RDVTabBarController
+//
+//  Created by Robert Dimitrov on 4/20/13.
+//  Copyright (c) 2013 Robert Dimitrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RDVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RDVAppDelegate class]));
+    }
+}
