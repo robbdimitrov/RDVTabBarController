9b45118a4b3c5838ce83fc8d30d683bdf589654d
Steffen Neubauer <stefreak@googlemail.com>
2014-11-30 03:52:28 +0100

    pass through preferredStatusBarStyle and preferredStatusBarUpdateAnimation requests
    Without these lines of code, the tab bar viewControllers cannot change the status bar style as easily - their preferredStatusBarStyle would never be called!

--- RDVTabBarController/RDVTabBarController.m
+++ RDVTabBarController/RDVTabBarController.m
@@ -58,6 +58,14 @@
     [self setTabBarHidden:self.isTabBarHidden animated:NO];
 }
 
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return self.selectedViewController.preferredStatusBarStyle;
+}
+
+- (UIStatusBarAnimation)preferredStatusBarUpdateAnimation {
+    return self.selectedViewController.preferredStatusBarUpdateAnimation;
+}
+
 - (NSUInteger)supportedInterfaceOrientations {
     UIInterfaceOrientationMask orientationMask = UIInterfaceOrientationMaskAll;
     for (UIViewController *viewController in [self viewControllers]) {
