c79c6e32925e9fcfc4208366bdc0a691588f8919
doggy <doggy8@gmail.com>
2014-12-11 14:41:26 +0800

    some updates for supporting auto-layout animation

--- RDVTabBarController/RDVTabBar.m
+++ RDVTabBarController/RDVTabBar.m
@@ -61,32 +61,35 @@
 }
 
 - (void)layoutSubviews {
-    CGSize frameSize = self.frame.size;
-    CGFloat minimumContentHeight = [self minimumContentHeight];
-    
-    [[self backgroundView] setFrame:CGRectMake(0, frameSize.height - minimumContentHeight,
-                                            frameSize.width, frameSize.height)];
-    
-    [self setItemWidth:roundf((frameSize.width - [self contentEdgeInsets].left -
-                               [self contentEdgeInsets].right) / [[self items] count])];
-    
-    NSInteger index = 0;
-    
-    // Layout items
-    
-    for (RDVTabBarItem *item in [self items]) {
-        CGFloat itemHeight = [item itemHeight];
+    // Is it exist Autolayout??
+    if (self.translatesAutoresizingMaskIntoConstraints) {
+        CGSize frameSize = self.frame.size;
+        CGFloat minimumContentHeight = [self minimumContentHeight];
         
-        if (!itemHeight) {
-            itemHeight = frameSize.height;
+        [[self backgroundView] setFrame:CGRectMake(0, frameSize.height - minimumContentHeight,
+                                                   frameSize.width, frameSize.height)];
+        
+        [self setItemWidth:roundf((frameSize.width - [self contentEdgeInsets].left -
+                                   [self contentEdgeInsets].right) / [[self items] count])];
+        
+        NSInteger index = 0;
+        
+        // Layout items
+        
+        for (RDVTabBarItem *item in [self items]) {
+            CGFloat itemHeight = [item itemHeight];
+            
+            if (!itemHeight) {
+                itemHeight = frameSize.height;
+            }
+            
+            [item setFrame:CGRectMake(self.contentEdgeInsets.left + (index * self.itemWidth),
+                                      roundf(frameSize.height - itemHeight) - self.contentEdgeInsets.top,
+                                      self.itemWidth, itemHeight - self.contentEdgeInsets.bottom)];
+            [item setNeedsDisplay];
+            
+            index++;
         }
-        
-        [item setFrame:CGRectMake(self.contentEdgeInsets.left + (index * self.itemWidth),
-                                  roundf(frameSize.height - itemHeight) - self.contentEdgeInsets.top,
-                                  self.itemWidth, itemHeight - self.contentEdgeInsets.bottom)];
-        [item setNeedsDisplay];
-        
-        index++;
     }
     
     [super layoutSubviews];
